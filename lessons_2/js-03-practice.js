935e7aa6cf2d47bcde1110dd753f5d3b9a4a6459
Vasylysaskyba <vasylysaskyba@gmail.com>
2024-03-04 00:30:19 +0200

    lessons-02-js
    operand, prompt, confirm

---
+++ lessons_2/js-03-practice.js
@@ -0,0 +1,23 @@
+// Приклад 1. Перевірка віку
+
+// const age = prompt("Скільки Вам років?");
+// const ageUser = Number(age); //рядок перетворюємо у число
+// if (isNaN(ageUser) === false) {
+//   if (ageUser <= 18) {
+//     alert("Ви не повнолітній!");
+//   } else {
+//     alert("Ви повнолітній!");
+//   }
+// } else {
+//   alert("Не правельні дані");
+// }
+
+// Приклад 1. Перевірка часу
+
+// const now = new Date();
+// const time = now.getHours();
+// if (time < 12) {
+//   alert("Перша половина дня");
+// } else {
+//   alert("Друга половина дня");
+// }
